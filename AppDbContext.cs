918f9bf853bed1951833d274cfa5a3f666c67308
isyahirah723 <isyahirah723@gmail.com>
2026-08-26 10:48:36 +0800

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UAT_System_API.Models;
+namespace UAT_System_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<TestCycles> TestCycles { get; set; }
+        public DbSet<TestCases> TestCases { get; set; }
+        public DbSet<TestSteps> TestSteps { get; set; }
+        public DbSet<TestFeedbacks> TestFeedbacks { get; set; }
+        public DbSet<ExecutionSteps> ExecutionSteps { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; }
+        public DbSet<ExecutionAttachments> ExecutionAttachments { get; set; }
+    }
+}
